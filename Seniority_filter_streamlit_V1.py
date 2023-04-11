d7f7b157d10cb720a11c1b3de0f536d7a6ad4dad
ConnectedCircles <130466104+ConnectedCircles@users.noreply.github.com>
2023-04-11 16:44:20 +0200

    Update Seniority_filter_streamlit_V1.py

--- Seniority_filter_streamlit_V1.py
+++ Seniority_filter_streamlit_V1.py
@@ -36,27 +36,16 @@ def app():
         # Filter to only include CXO+, delete CXO+ column
         dffiltered = df[df["CXO+"]=="Yes"]
         dffiltered = dffiltered.drop("CXO+", axis=1)
-
+        
+        # Create a new dataframe with only the "Profile URL" column for the filtered data
+        url_df = dffiltered[["Profile URL"]]
+        
         # Download link for filtered data
         csv_filtered = dffiltered.to_csv(index=False)
         b64_filtered = base64.b64encode(csv_filtered.encode('utf-8')).decode()
         href_filtered = f'<a href="data:file/csv;base64,{b64_filtered}" download="filtered_data.csv">Download Filtered CSV File</a>'
         
-        # Download link for unfiltered data
-        csv_unfiltered = df.to_csv(index=False)
-        b64_unfiltered = base64.b64encode(csv_unfiltered.encode('utf-8')).decode()
-        href_unfiltered = f'<a href="data:file/csv;base64,{b64_unfiltered}" download="unfiltered_data.csv">Download Unfiltered CSV File</a>'
-
-        # Display both filtered and unfiltered data in two windows
-        col1, col2 = st.beta_columns(2)
-        with col1:
-            st.write("Unfiltered Data")
-            st.write(df)
-            st.markdown(href_unfiltered, unsafe_allow_html=True)
-        with col2:
-            st.write("Filtered Data")
-            st.write(dffiltered)
-            st.markdown(href_filtered, unsafe_allow_html=True)
-
-if __name__ == "__main__":
-    app()
+        # Download link for "Profile URL" column of filtered data
+        csv_url = url_df.to_csv(index=False, header=False)
+        b64_url = base64.b64encode(csv_url.encode('utf-8')).decode()
+        href_url = f'<a href="data:file/csv;base64,{b64_url}" download="filtered_urls.csv">Download Filtered Profile URLs</a>'
