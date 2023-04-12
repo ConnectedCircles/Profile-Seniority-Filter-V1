9118bf48da3da7e4404dad70778953ca29ccb4d0
ConnectedCircles <130466104+ConnectedCircles@users.noreply.github.com>
2023-04-12 09:56:51 +0200

    Update Seniority_filter_streamlit_V1.py

--- Seniority_filter_streamlit_V1.py
+++ Seniority_filter_streamlit_V1.py
@@ -7,9 +7,12 @@ def app():
     # Set title and subtitle, additional text
     st.title("Seniority Filter V2")
     st.subheader("Property of Connected Circles")
-    st.text("This app allows you to filter lists of profiles by seniority. By default, it uses a set of keywords to detect and filter CXO+ level profiles (including partners and VPs)")
-    st.text("It uses 2 sets of keywords, one that is case-sensitive and one that is case insensitive. This avoid errors such as the  inclusion of 'aCCOunt managers' when searching for 'CCO'")
-    st.text("Both sets of keywords are fully customizable and keywords can be added or removed. Keywords must be separated by a comma, whitespace will be considered a part of a keyword.")
+    st.text("This app allows you to filter lists of profiles by seniority. By default, it \n
+    uses a set of keywords to detect and filter CXO+ level profiles (incl. partners and VPs)\n
+    It uses 2 sets of keywords, one that is case-sensitive and one that is case insensitive.\n
+    This avoids errors such as the  inclusion of 'aCCOunt managers' when searching for 'CCO'")\n
+    "Both sets of keywords are fully customizable and keywords can be added or removed.\n
+    Keywords must be separated by a comma, whitespace will be considered a part of a keyword.")
     
     
     # Define the list of substrings to search for
